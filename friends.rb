ecb5af81c02769362a4392c317f0b20b07d13aec
pcbennett108 <pcbennett42@gmail.com>
2023-04-12 14:20:47 -0600

    Add array method and new array

--- friends.rb
+++ friends.rb
@@ -1,6 +1,10 @@
 friends = ["bob", "sally", "steve", "neo", "amy"]
+places = ["Rome", "Agra", "Istanbull", "Giza", "Paris"]
+
 
 puts friends[2]
 puts friends[-1]
 puts friends[0]
 
+places.push("London")
+p places
